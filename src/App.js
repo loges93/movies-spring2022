197a54731762311be0c66d623320e5be5067d79e
loges93 <58162404+loges93@users.noreply.github.com>
2022-04-04 20:13:03 -0500

    Added name

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {movies} from './movies';
 const App = () => {
     return (
         <div>
+            <p>Logan Riedell</p>
             <pre>{JSON.stringify(movies, null, 4)}</pre>
         </div>
     );
